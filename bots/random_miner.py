a10679a5150d09c22ec1291477078c0a40685a59
Juan Pedro Fisanotti <fisadev@gmail.com>
2026-03-21 11:25:17 -0300

    Use constants

--- bots/random_miner.py
+++ bots/random_miner.py
@@ -1,6 +1,6 @@
 import random
 
-from tv.game import Position
+from tv.game import Position, POWER_TO, FLY_TO, LASERS, ENGINES, SHIELDS, ASTEROID
 
 
 class BotLogic:
@@ -18,25 +18,25 @@ class BotLogic:
         This bot looks for asteroids, and runs home when it has one.
         """
         # speed is life
-        desired_distribution = {"engines": 3, "shields": 0, "lasers": 0}
+        desired_distribution = {ENGINES: 3, SHIELDS: 0, LASERS: 0}
         if power_distribution != desired_distribution:
-            return "power_to", desired_distribution
+            return POWER_TO, desired_distribution
 
-        speed = power_distribution["engines"] - cargo
+        speed = power_distribution[ENGINES] - cargo
 
         if cargo:
             # run home
             home_base_position = Position(0, 0)
             reacheable_positions = list(position.positions_in_range(speed))
             closest_to_home = min(reacheable_positions, key=lambda p: p.distance_to(home_base_position))
-            return "fly_to", closest_to_home
+            return FLY_TO, closest_to_home
         else:
             for contact_pos, contact_type in radar_contacts.items():
-                if contact_type == "asteroid":
+                if contact_type == ASTEROID:
                     # fly to the first asteroid we see
                     reacheable_positions = list(position.positions_in_range(speed))
                     closest_to_asteroid = min(reacheable_positions, key=lambda p: p.distance_to(contact_pos))
-                    return "fly_to", closest_to_asteroid
+                    return FLY_TO, closest_to_asteroid
             # explore randomly
-            return "fly_to", random.choice(list(position.positions_in_range(1)))
+            return FLY_TO, random.choice(list(position.positions_in_range(1)))
 
